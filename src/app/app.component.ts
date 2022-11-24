02ed11ef77ad3e41e328084a866e974eb3e06078
unknown <singhkuvir003@gmail.com>
2022-11-24 20:45:21 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Student{
+  id: number;
+  name: string;
+  country: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-search-filter';
+
+  searchText: any;
+
+  students: Student[] = [
+    {
+      id: 1,
+      name: "kulvir",
+      country: "India"
+    },
+    {
+      id: 2,
+      name: "harman",
+      country: "India"
+    },
+    {
+      id: 3,
+      name: "burhan",
+      country: "India"
+    },
+    {
+      id: 4,
+      name: "shamim",
+      country: "India"
+    },
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    Ng2SearchPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
